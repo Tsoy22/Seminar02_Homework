3f1f2c5e5db0990b14872a3ca29702067cf0ea8b
Vitaly Tsoy <tsoyvitalik@icloud.com>
2022-09-14 20:21:49 +0300

    task10

---
+++ task10/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456-> 5
+// 782 -> 8
+// 918 -> 1
+
+// Console.Write("Введите 3-х значное число: ");
+// string? number = Console.ReadLine();
+
+// Console.WriteLine($"Вторая цифра числа {number} это - {number[1]}");
+
+Console.Write("Введите 3-х значное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int firstDigit = number / 10;
+int secondDigit = firstDigit % 10;
+Console.WriteLine($"Вторая цифра числа {number} - это {secondDigit} ");
