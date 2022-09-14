9e6208a8a3eacd6cc4a45245cf450893a19a6883
Vitaly Tsoy <tsoyvitalik@icloud.com>
2022-09-14 20:50:06 +0300

    task13

---
+++ task13/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78->третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99)
+{
+  string str = number.ToString();
+  Console.WriteLine($"Вторая цифра числа {number} это - {str[1]}");
+}
+else
+{
+  Console.WriteLine("Нет третьей цифры");
+}
